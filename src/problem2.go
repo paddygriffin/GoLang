8cd9bae9a5bff7a0d9b34ddde1bbe8b56999eee5
paddygriffin <paddygriffin1995@gmail.com>
2016-09-19 20:15:58 +0100

    need to fix problem 2

--- src/problem2.go
+++ src/problem2.go
@@ -10,25 +10,66 @@ import (
 //a prime number is a number that can divide by itself and 1
 
 func main() {
-	var num int=10001
-	var primeNum  = " "
+	/*var num int
+	var primeNum int
+	var counter int = 0 //1
 
 	for i := 0; i < 10001; i++{
-		var counter int = 0
-		for num=i; num>=1; num-- {
-			if i % num == 0 {
-				counter = counter + 1;
-			}
+		i++
+
+		if (i % i == 0) && (i % 1 == 0){
+			counter = counter + 1;
 		}
-			if counter ==2{
-				primeNum = primeNum + i + " "
-			}
+
+		if i ==2{
+			counter++
+		}
 
 
 
 		}
 	fmt.Println("the prime number: ", primeNum)
-	}
+
+	//variable
+
+	//continuos loop until 10001
+	//inside loop for checking
+
+	//check if num is prime
+
+	//print out the amount of prime numbers*/
+
+//	var i int //loop 1
+	var j int // loop 2
+	var primeNumCount int //counting the prime
+
+	for i := 1; i < 10001; i++{
+		i++ //increment
+		//checking
+		if(i%1==0)&&(i%i==0){
+			if i==2{
+				primeNumCount++
+			}else{
+				for j =2;j<i;j++{
+					if i % j ==0{
+						break
+					}else if j == i-1 {
+						primeNumCount++
+						if primeNumCount == 1001{
+							primeNumCount = i
+						}
+					}else {
+						continue
+					}
+				}
+			}
+
+
+		}//if
+
+	}//for
+	fmt.Println("the number:", primeNumCount)
+}
 
 
 
